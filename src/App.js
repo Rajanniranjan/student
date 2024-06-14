5b0c6dad543fb0493aa65562e8a85987852c1be0
Rajan Niranjan <rajanniranjan456@gmail.com>
2024-06-14 00:18:17 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+ 
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import ResumeApp from './resume_builder/ResumeApp';
+import TodoApp from './Todolist/TodoApp';
+import NoteApp from './NotesApp/NoteApp';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <nav className="App-nav">
+          <ul className="App-nav-list">
+            <li><Link to="/" className="App-button">Home</Link></li>
+            <li><Link to="/resume" className="App-button">ResumeApp</Link></li>
+            <li><Link to="/todo" className="App-button">TodoApp</Link></li>
+            <li><Link to="/noteapp" className="App-button">Notes</Link></li>
+          </ul>
+        </nav>
+        
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/resume" element={<ResumeApp />} />
+          <Route path="/todo" element={<TodoApp />} />
+          <Route path="/noteapp" element={<NoteApp />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
